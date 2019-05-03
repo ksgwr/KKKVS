1556b378331080e70efec69e783a61856b496517
ksgwr <ksgwarai@gmail.com>
2019-05-03 14:50:33 +0900

    add ssl flag

--- travis/build-wheels-using-mac.sh
+++ travis/build-wheels-using-mac.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e -x
 
+export CPPFLAGS=-I$(brew --prefix openssl)/include
+export LDFLAGS=-L$(brew --prefix openssl)/lib
+
 readonly ROOT_DIR=$(cd $(dirname $0)/../; pwd)
 readonly VERSIONS=(3.4-dev 3.5-dev 3.6-dev 3.7-dev 3.8-dev)
 
