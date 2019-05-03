f99231c323ad79ce88180e3382e708a201bccaba
ksgwr <ksgwarai@gmail.com>
2019-05-03 13:46:40 +0900

    add mac build sh

--- travis/build-wheels-using-docker.sh
+++ travis/build-wheels-using-docker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e -x
 
+readonly ROOT_DIR=$(cd $(dirname $0)/../; pwd)
+
 # install require library
 /opt/python/cp35-cp35m/bin/pip install cmake
 yum install gcc-c++ -y
 
 # build basic
-rm -rf /io/build
-mkdir -p /io/build
-cd /io/build
+mkdir -p ${ROOT_DIR}/build
+cd ${ROOT_DIR}/build
 /opt/python/cp35-cp35m/bin/cmake ..
 make
 make test ARGS='-V'
@@ -16,18 +17,12 @@ cd python
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/python/requirements-dev.txt
+    "${PYBIN}/pip" install -r ${ROOT_DIR}/python/requirements-dev.txt
     rm -rf build
-    "${PYBIN}/python" /io/python/setup.py bdist_wheel
+    "${PYBIN}/python" ${ROOT_DIR}/python/setup.py test bdist_wheel
 done
 
 # Bundle external shared libraries into the wheels
 for whl in dist/*.whl; do
     auditwheel repair "$whl" --plat $PLAT -w wheelhouse/
-done
-
-# Install packages and test
-for PYBIN in /opt/python/*/bin/; do
-    rm -rf build
-    "${PYBIN}/python" /io/python/setup.py test
 done
---
+++ travis/build-wheels-using-mac.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e -x
+
+readonly ROOT_DIR=$(cd $(dirname $0)/../; pwd)
+readonly VERSIONS=(3.4-dev 3.5-dev 3.6-dev 3.7-dev 3.8-dev)
+
+# install require library
+for version in ${VERSIONS[@]}; do
+    if [ ! -d ~/.pyenv/versions/$version ]; then
+        pyenv install $version
+    fi
+done
+
+# build basic
+mkdir -p ${ROOT_DIR}/build
+cd ${ROOT_DIR}/build
+cmake ..
+make
+make test ARGS='-V'
+cd python
+
+# Compile wheels
+for version in ${VERSIONS[@]}; do
+    pyenv local $version
+    pip install -r $ROOT_DIR/python/requirements-dev.txt
+    find $ROOT_DIR/python | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+    python $ROOT_DIR/python/setup.py test bdist_wheel
+done
