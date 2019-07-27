60a181c2e8d8534f146afb5b88dd48d8f82e1e2a
ksgwr <ksgwarai@gmail.com>
2019-07-28 02:24:51 +0900

    remove debug info

--- travis/build-wheels-using-mac.sh
+++ travis/build-wheels-using-mac.sh
@@ -4,12 +4,13 @@ set -e -x
 readonly ROOT_DIR=$(cd $(dirname $0)/../; pwd)
 readonly VERSIONS=(3.4-dev 3.5-dev 3.6-dev 3.7-dev 3.8-dev)
 
+eval "$(pyenv init -)"
 # install require library
-#for version in ${VERSIONS[@]}; do
-#    if [ ! -d ~/.pyenv/versions/$version ]; then
-#        pyenv install $version
-#    fi
-#done
+for version in ${VERSIONS[@]}; do
+    if [ ! -d ~/.pyenv/versions/$version ]; then
+        pyenv install $version
+    fi
+done
 
 # build basic
 mkdir -p ${ROOT_DIR}/build
@@ -19,14 +20,9 @@ make
 make test ARGS='-V'
 cd python
 
-eval "$(pyenv init -)"
 # Compile wheels
 for version in ${VERSIONS[@]}; do
-    pyenv install $version
     export PYENV_VERSION=$version
-    which pyenv
-    pyenv versions
-    python --version
     python -m pip install -r $ROOT_DIR/python/requirements-dev.txt
     find $ROOT_DIR/python | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
     python $ROOT_DIR/python/setup.py test bdist_wheel
