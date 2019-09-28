f7229410613691d24e2b7ac2dceb2251a362ee6a
ksgwr <ksgwarai@gmail.com>
2019-09-28 16:45:35 +0900

    fix

--- ci/build-wheels-using-windows.bat
+++ ci/build-wheels-using-windows.bat
@@ -1,3 +1,10 @@
+set PLATFORM=%1
+if "%PLATFORM%"=="" set PLATFORM=x64
+set PLATFORM_PREFIX=
+if "%PLATFORM%"=="x64" set PLATFORM_PREFIX=-x64
+set _CL_=/utf-8
+set PATH=c:\Python37%PLATFORM_PREFIX%;c:\Program Files\Git\usr\bin;c:\MinGW\bin;%PATH%
+
 mkdir build
 cd build
 cmake ..
