4bcd03ebdc9ac7b21a2dc367a3b43908d25ff6ea
ksgwr <ksgwarai@gmail.com>
2019-10-05 18:43:38 +0900

    add python version

--- ci/build-wheels-using-windows.bat
+++ ci/build-wheels-using-windows.bat
@@ -4,9 +4,22 @@ set PLATFORM_PREFIX=
 if "%PLATFORM%"=="x64" set PLATFORM_PREFIX=-x64
 set _CL_=/utf-8
 set PATH=c:\Python37%PLATFORM_PREFIX%;c:\Program Files\Git\usr\bin;c:\MinGW\bin;%PATH%
+set ROOT_DIR=%~dp0
 
 mkdir build
 cd build
 cmake .. -A %PLATFORM%
 cmake --build . --config Release
 ctest -C Release
+cd python
+
+call :BuildPython C:\Python35%PLATFORM_PREFIX%
+call :BuildPython C:\Python36%PLATFORM_PREFIX%
+call :BuildPython C:\Python37%PLATFORM_PREFIX%
+
+:BuildPython
+%1\python -m pip install -r %ROOT_DIR%\python\requirements-dev.txt
+rmdir /Q /S build
+del /S *.pyd
+%1\python setup.py bdist_wheel
+exit /b
