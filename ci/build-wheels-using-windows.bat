2ac46fc28416bf2758220f6df2cf03c1ff366eae
ksgwr <ksgwarai@gmail.com>
2019-10-15 22:13:13 +0900

    add debug code

--- ci/build-wheels-using-windows.bat
+++ ci/build-wheels-using-windows.bat
@@ -10,7 +10,7 @@ mkdir build
 cd build
 cmake .. -A %PLATFORM%
 cmake --build . --config Release || goto :error
-ctest -C Release || goto :error
+ctest -C Release --verbose || goto :error
 cd python
 
 rem https://www.appveyor.com/docs/windows-images-software/#python
@@ -30,5 +30,5 @@ del /S *.pyd
 exit /b
 
 :error
-rem exit /b %errorlevel%
+exit /b %errorlevel%
 
